18dcd9240889115825244279dd724dce19c54667
Robert Si <robertsi0214@gmail.com>
2019-11-17 13:35:07 -0800

    complete first bubble sort solution

---
+++ dailyalgorithms/src/bubbleSort.js
@@ -0,0 +1,16 @@
+//Bubble Sort
+//implement bubble sort algorithm
+
+
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        let tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+    }
+  }
+  return array;
+}
